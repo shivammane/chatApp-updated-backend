4dfbbcfa84321d740b662cbbbcaf3b04235f5a61
shivam mane <shivam@mail.com>
2023-02-13 17:15:15 +0530

    updated login logic in login_controller and removed login_middleware

--- src/controllers/login_controller.js
+++ src/controllers/login_controller.js
@@ -4,11 +4,10 @@ module.exports = (req, res) => {
     const data = req.body;
     mysql.query(
         `select concat(firstname,' ',lastname) as name,email,userid from users
-    where email="${data.email}"`,
+    where email="${data.email}" and password="${data.password}"`,
         (err, result) => {
             if (err) throw err;
-
-            res.send(result);
+            result.length != 0 ? res.send(result) : res.send([{ email: "false" }]);
         }
     );
 };
--- src/routes/login_route.js
+++ src/routes/login_route.js
@@ -1,7 +1,6 @@
 const router = require("express").Router();
-const login_validator = require("../middleware/login_middleware");
 const login = require("../controllers/login_controller");
 
-router.post("/login", login_validator, login);
+router.post("/login", login);
 
 module.exports = router;
