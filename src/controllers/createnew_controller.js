3697ad27cc4f3d3430d47b04198069f2176376ee
shivam mane <shivam@mail.com>
2023-02-14 13:36:10 +0530

    removed console.log statements from createnew_controller.js

--- src/controllers/createnew_controller.js
+++ src/controllers/createnew_controller.js
@@ -2,11 +2,9 @@ const mysql = require("../controllers/mysql_connector");
 
 module.exports = (req, res) => {
     const data = req.body;
-    console.log(data);
     sql = `INSERT INTO users (firstname,lastname,email,password) VALUES ("${data.firstname}","${data.lastname}","${data.email}","${data.password}")`;
     mysql.query(sql, (err, result) => {
         if (err) {
-            console.log(err);
             res.send("false");
             return;
         }
